806642776d08136ca310fbc04a782bea61477b8a
jxstxn1 <justinbaumann12112001@gmail.com>
2020-02-09 14:08:06 +0100

    bigger buttons

--- src/pack1/ButtonPlacement.java
+++ src/pack1/ButtonPlacement.java
@@ -5,53 +5,53 @@ public class ButtonPlacement {
     public static void place(){
 
         //Reihe1
-        Gui.btn[0].setBounds(175,575,50,50);
-        Gui.btn[1].setBounds(275,575,50,50);
-        Gui.btn[2].setBounds(375,575,50,50);
-        Gui.btn[3].setBounds(475,575,50,50);
-        Gui.btn[4].setBounds(575,575,50,50);
-        Gui.btn[5].setBounds(675,575,50,50);
-        Gui.btn[6].setBounds(775,575,50,50);
+        Gui.btn[0].setBounds(150,550,100,100);
+        Gui.btn[1].setBounds(250,550,100,100);
+        Gui.btn[2].setBounds(350,550,100,100);
+        Gui.btn[3].setBounds(450,550,100,100);
+        Gui.btn[4].setBounds(550,550,100,100);
+        Gui.btn[5].setBounds(650,550,100,100);
+        Gui.btn[6].setBounds(750,550,100,100);
         //Reihe2
-        Gui.btn[7].setBounds(175,475,50,50);
-        Gui.btn[8].setBounds(275,475,50,50);
-        Gui.btn[9].setBounds(375,475,50,50);
-        Gui.btn[10].setBounds(475,475,50,50);
-        Gui.btn[11].setBounds(575,475,50,50);
-        Gui.btn[12].setBounds(675,475,50,50);
-        Gui.btn[13].setBounds(775,475,50,50);
+        Gui.btn[7].setBounds(150,450,100,100);
+        Gui.btn[8].setBounds(250,450,100,100);
+        Gui.btn[9].setBounds(350,450,100,100);
+        Gui.btn[10].setBounds(450,450,100,100);
+        Gui.btn[11].setBounds(550,450,100,100);
+        Gui.btn[12].setBounds(650,450,100,100);
+        Gui.btn[13].setBounds(750,450,100,100);
         //Reihe3
-        Gui.btn[14].setBounds(175,375,50,50);
-        Gui.btn[15].setBounds(275,375,50,50);
-        Gui.btn[16].setBounds(375,375,50,50);
-        Gui.btn[17].setBounds(475,375,50,50);
-        Gui.btn[18].setBounds(575,375,50,50);
-        Gui.btn[19].setBounds(675,375,50,50);
-        Gui.btn[20].setBounds(775,375,50,50);
+        Gui.btn[14].setBounds(150,350,100,100);
+        Gui.btn[15].setBounds(250,350,100,100);
+        Gui.btn[16].setBounds(350,350,100,100);
+        Gui.btn[17].setBounds(450,350,100,100);
+        Gui.btn[18].setBounds(550,350,100,100);
+        Gui.btn[19].setBounds(650,350,100,100);
+        Gui.btn[20].setBounds(750,350,100,100);
         //Reihe4
-        Gui.btn[21].setBounds(175,275,50,50);
-        Gui.btn[22].setBounds(275,275,50,50);
-        Gui.btn[23].setBounds(375,275,50,50);
-        Gui.btn[24].setBounds(475,275,50,50);
-        Gui.btn[25].setBounds(575,275,50,50);
-        Gui.btn[26].setBounds(675,275,50,50);
-        Gui.btn[27].setBounds(775,275,50,50);
+        Gui.btn[21].setBounds(150,250,100,100);
+        Gui.btn[22].setBounds(250,250,100,100);
+        Gui.btn[23].setBounds(350,250,100,100);
+        Gui.btn[24].setBounds(450,250,100,100);
+        Gui.btn[25].setBounds(550,250,100,100);
+        Gui.btn[26].setBounds(650,250,100,100);
+        Gui.btn[27].setBounds(750,250,100,100);
         //Reihe5
-        Gui.btn[28].setBounds(175,175,50,50);
-        Gui.btn[29].setBounds(275,175,50,50);
-        Gui.btn[30].setBounds(375,175,50,50);
-        Gui.btn[31].setBounds(475,175,50,50);
-        Gui.btn[32].setBounds(575,175,50,50);
-        Gui.btn[33].setBounds(675,175,50,50);
-        Gui.btn[34].setBounds(775,175,50,50);
+        Gui.btn[28].setBounds(150,150,100,100);
+        Gui.btn[29].setBounds(250,150,100,100);
+        Gui.btn[30].setBounds(350,150,100,100);
+        Gui.btn[31].setBounds(450,150,100,100);
+        Gui.btn[32].setBounds(550,150,100,100);
+        Gui.btn[33].setBounds(650,150,100,100);
+        Gui.btn[34].setBounds(750,150,100,100);
         //Reihe6
-        Gui.btn[35].setBounds(175,75,50,50);
-        Gui.btn[36].setBounds(275,75,50,50);
-        Gui.btn[37].setBounds(375,75,50,50);
-        Gui.btn[38].setBounds(475,75,50,50);
-        Gui.btn[39].setBounds(575,75,50,50);
-        Gui.btn[40].setBounds(675,75,50,50);
-        Gui.btn[41].setBounds(775,75,50,50);
+        Gui.btn[35].setBounds(150,50,100,100);
+        Gui.btn[36].setBounds(250,50,100,100);
+        Gui.btn[37].setBounds(350,50,100,100);
+        Gui.btn[38].setBounds(450,50,100,100);
+        Gui.btn[39].setBounds(550,50,100,100);
+        Gui.btn[40].setBounds(650,50,100,100);
+        Gui.btn[41].setBounds(750,50,100,100);
 
 
     }
